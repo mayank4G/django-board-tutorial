d679edc5528f834b0cb2c31094b138e5d5b63d24
mayank4G <porwal.mayank075@gmail.com>
2022-03-16 17:21:38 +0530

    Newly added all password templates

--- myproject/urls.py
+++ myproject/urls.py
@@ -28,6 +28,18 @@ urlpatterns = [
     path('profile/(<username>[\w.@+-]+)/', views.user_profile, name='user_profile'),
     path('signup/', accounts_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('login/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html',
+        email_template_name = 'password_reset_email.html',
+        subject_template_name = 'password_reset_subject.txt'
+        ), name='password_reset'),
+    path('reset/done', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/(<uidb64>[0-9A-Za-z_\-]+)/(<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html'
+        ), name='password_reset_confirm'),
+    path('reset/complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'
+        ), name='password_reset_complete'),
     path('admin/', admin.site.urls),
 ] 
